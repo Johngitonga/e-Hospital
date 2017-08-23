8d0f4b759d185d915ceb9f530f0a212fea257aa2
Jk24600205 <ipovigitonga@gmail.com>
2017-08-23 16:55:06 +0300

    sign up/login forms added

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>e-Hospital</title>
+</head>
+<body>
+
+<form method = "POST" action = "index.php">
+  Username: <input type = "text" name = "username" placeholder="Enter your username">
+</br>
+  Password: <input type = "password" name = "password" placeholder="Enter your password">
+</br>
+<input type = "submit" name = "submit" value = "Login">
+</form>
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>e-Hospital</title>
+</head>
+<body>
+
+<form method = "POST" action = "index.php">
+  First Name: <input type = "text" name = "First Name" placeholder="Enter your First Name">
+</br>
+Second Name: <input type = "text" name = "Second Name" placeholder="Enter your Second Name">
+</br>
+Gender: <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+Username or Email: <input type = "text" name = "username or email" placeholder="Enter your Username or Email">
+</br>
+  Password: <input type = "password" name = "password" placeholder="Enter your password">
+</br>
+Confirm Password: <input type = "password" name = "confirm password" placeholder="Enter your password">
+</br>
+
+<input type = "checkbox"> Confirm you have read and understood our user agreement
+</br>
+<input type = "submit" name = "submit" value = "Submit">
+</form>
+
+</body>
+</html>

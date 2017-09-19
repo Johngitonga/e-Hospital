2f8e4784694a757e7bec0d80167e913f171f7f1b
Jk24600205 <ipovigitonga@gmail.com>
2017-09-19 15:48:59 +0300

    pushing the main.css and signup.php

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>e-Hospital</title>
+</head>
+<body>
+<ul style="list-style-type:none">
+  <li>Patient Details</li>
+  <li>Pharmacy</li>
+  <li>Hospital Staff</li>
+</ul>
+
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -2,27 +2,37 @@
 <html>
 <head>
 <title>e-Hospital</title>
+<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 
+<div id="placeholder">
 <form method = "POST" action = "index.php">
-  First Name: <input type = "text" name = "First Name" placeholder="Enter your First Name">
+
+First Name<br>
+<input type = "text" name = "First Name" placeholder="Enter your First Name">
 </br>
-Second Name: <input type = "text" name = "Second Name" placeholder="Enter your Second Name">
+Second Name <br>
+<input type = "text" name = "Second Name" placeholder="Enter your Second Name">
 </br>
-Gender: <input type="radio" name="gender" value="male" checked> Male<br>
+Gender<br>
+<input type="radio" name="gender" value="male" checked> Male<br>
   <input type="radio" name="gender" value="female"> Female<br>
-Username or Email: <input type = "text" name = "username or email" placeholder="Enter your Username or Email">
+Username or Email <br>
+ <input type = "text" name = "username or email" placeholder="Enter your Username or Email">
 </br>
-  Password: <input type = "password" name = "password" placeholder="Enter your password">
+  Password <br>
+  <input type = "password" name = "password" placeholder="Enter your password">
 </br>
-Confirm Password: <input type = "password" name = "confirm password" placeholder="Enter your password">
+Confirm Password <br>
+<input type = "password" name = "confirm password" placeholder="Enter your password">
 </br>
 
 <input type = "checkbox"> Confirm you have read and understood our user agreement
 </br>
 <input type = "submit" name = "submit" value = "Submit">
 </form>
+</div>
 
 </body>
 </html>
